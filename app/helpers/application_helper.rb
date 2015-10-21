b352e19110e246fb0aab0b9e5d9dca2faa96fec9
Danielle Giglia <daniellegiglia@yahoo.com>
2015-10-21 01:21:45 +0000

    Fixed some display issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
   def rating_options(selected=nil)
     options_for_select((1..5).map { |rating| [pluralize(rating, "Bite"), rating]}, selected)
   end
+  
+  def latest_chomp(obj)
+    return "N/A" if obj.reviews.empty?
+    display_datetime(obj.reviews.last.created_at)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password validations: false
   
-  validates_presence_of :email, :password, :name
+  validates_presence_of :email, :password, :name, :city, :state
   validates_uniqueness_of :email 
 
 end
