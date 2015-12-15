42d1ed90cbd80fa79c336c1410ffdabeb9587cc6
Danielle Giglia <daniellegiglia@yahoo.com>
2015-12-15 23:52:21 +0000

    update spec for favorite, begin spec for follow

--- spec/features/user_favorites_business_spec.rb
+++ spec/features/user_favorites_business_spec.rb
@@ -13,6 +13,14 @@ feature "User favorites business" do
     click_on "Favorite"
     visit favorites_path
     
-    expect(page).to have_content("Funville")
+    expect(page).to have_content "Funville"
+    
+    unfavorite_business
+    visit favorites_path
+    expect(page).not_to have_content "Funville"    
+  end
+  
+  def unfavorite_business
+    find("a[data-method='delete']").click
   end
 end
