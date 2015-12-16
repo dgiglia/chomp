4ece8d23627005afa39e295aa8dedd12a28d8868
Danielle Giglia <daniellegiglia@yahoo.com>
2015-12-16 03:08:43 +0000

    UI updates

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -8,5 +8,23 @@ class Favorite < ActiveRecord::Base
   def category_name
     category.name
   end
+  
+  def rating
+    review.rating if review
+  end
+  
+  def rating=(new_rating)    
+    if review
+      review.update_column(:rating, new_rating)
+    else
+      review = Review.new(user: user, business: business, rating: new_rating)
+      review.save(validate: false)
+    end
+  end
+  
+  private
+  def review
+    @review ||= Review.find_by(user_id: user.id, business_id: business.id)
+  end
     
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -21,4 +21,42 @@ describe Favorite do
       expect(favorite.category).to eq(category)
     end
   end
+  
+  describe "#rating" do
+    let(:business) {Fabricate(:business)}
+    let(:user) {Fabricate(:user)}
+    let(:favorite) {Fabricate(:favorite, user: user, business: business)}
+    
+    it "returns review rating if there is a review" do
+      review = (Fabricate :review, user: user, business: business, rating: 4)
+      expect(favorite.rating).to eq(4)
+    end
+    
+    it "returns nil if there is no review" do
+      expect(favorite.rating).to eq(nil)
+    end
+  end
+  
+  describe "#rating=" do
+    let(:business) {Fabricate(:business)}
+    let(:user) {Fabricate(:user)}
+    let(:item) {Fabricate(:favorite, user: user, business: business)}
+    
+    it "changes the rating if review is present" do
+      review = Fabricate(:review, user: user, business: business, rating: 2)
+      item.rating = 4
+      expect(item.reload.rating).to eq(4)
+    end
+    
+    it "clears rating if review is present" do
+      review = Fabricate(:review, user: user, business: business, rating: 2)
+      item.rating = nil
+      expect(item.reload.rating).to be_nil
+    end
+    
+    it "creates review with rating if review is not present" do
+      item.rating = 4
+      expect(item.reload.rating).to eq(4)
+    end
+  end
 end
