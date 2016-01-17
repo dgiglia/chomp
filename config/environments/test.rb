710d43ac9f5aad2418d024380b6cb2fc0cf7779e
Danielle Giglia <daniellegiglia@yahoo.com>
2016-01-17 02:45:22 +0000

    test env update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,7 @@ Chomp::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = { host: 'localhost:52662' }
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
